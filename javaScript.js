b9b0af237df68ad39b1ec853e09042c040aacbe1
FrankCobby <frankcobby7@gmail.com>
2015-09-22 13:10:02 +0100

    jsfiddle

---
+++ javaScript.js
@@ -0,0 +1,13 @@
+var face = $("#face");
+//var element = document.getElementById("#tongue");
+var tongue =$("#tongue");
+
+face.click(function(){
+    //element.style.display = 'none';
+    //alert("Don't touch my tongue!");
+    
+    
+    tongue.attr("visibility","hidden");
+    
+});
+
